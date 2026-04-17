8f5b5ab0a85624817835a400fd256eecf5dbca4e
Adit <aditjr427@gmail.com>
2026-04-17 10:50:20 -0700

    perubahan

--- aa.php
+++ aa.php
@@ -1,3 +1,29 @@
 <?php
 echo "Hello, world!";
+
+
+echo "<br><br>=================<br><br>";
+
+$nama = "adit";
+$umur = 20;
+$setatus = "mahasiswa";
+
+
+echo "nama saya $nama, umur saya $umur, saya seorang $setatus";
+
+echo "<br><br>=================<br><br>";
+
+$nilai = 75;
+$nilai = 70;
+
+
+if ($nilai >= 70) {
+    echo "lulus";
+}
+else {
+    echo "tidak lulua";
+}
+for ($i = 1; $i <=7; $i++) {
+    echo "angka ke $i";
+}
 ?>
