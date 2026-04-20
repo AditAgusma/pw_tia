df8f4ab307b1c73de62ded6dd869634df605e614
Adit <aditjr427@gmail.com>
2026-04-21 02:26:57 +0700

    update

--- materi2.php
+++ materi2.php
@@ -8,9 +8,9 @@ if(isset ($_POST["angka"])){
     $newAngka = $_POST["angka"];
     for ($i = 1; $i <= $newAngka; $i++) {
         if ($i % 2 == 0) {
-            echo "$i ini adlah angka genap <br>";
+            echo "$i ini adlah angka (genap) <br>";
         }else {
-            echo "$i ini adalah angka ganjil <br>";
+            echo "$i ini adalah angka (ganjil) <br>";
         }
     }
 }
