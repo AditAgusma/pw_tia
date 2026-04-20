db4250114d65aed84cb53b663d2ea84d467f13b6
AditAgusma <aditjr427@gmail.com>
2026-04-20 12:23:48 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+echo "br><br><a href='materi1.php'materi 1 : if dan else</a>";
+echo "br><br><a href='materi2.php'materi 2 : looping</a>";
+echo "br><br><a href='materi3.php'materi 3 : looping</a>";
+echo "br><br><a href='materi3.php'materi 3 : looping</a>";
+echo "br><br><a href='materi4.php'materi 4 : looping</a>";
+echo "br><br><a href='materi4.php'materi 4 : looping</a>";
+
+?>
---
+++ materi2.php
@@ -0,0 +1,18 @@
+<form method="post">
+    masukan angka : <input type="number" name="angka">
+    <input type="submit" name="kirm" value="kirim">
+</form>
+
+<?php
+if(isset ($_POST["angka"])){
+    $newAngka = $_POST["angka"];
+    for ($i = 1; $i <= $newAngka; $i++) {
+        if ($i % 2 == 0) {
+            echo "$i ini adlah angka genap <br>";
+        }else {
+            echo "$i ini adalah angka ganjil <br>";
+        }
+    }
+}
+
+?>
