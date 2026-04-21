45fdbff71a0a19134dbca007b1ac5e93ea3affcf
Adit <aditjr427@gmail.com>
2026-04-21 14:07:20 +0700

    log in

--- materi2.php
+++ materi2.php
@@ -1,18 +1,20 @@
+<!DOCTYPE HTML>
+<HTML>
+    <HEAD>
+        <title>login</title>
+</HEAD>
+
+<BODY>
+
+
+
 <form method="post">
-    masukan angka : <input type="number" name="angka">
-    <input type="submit" name="kirm" value="kirim">
+    username : <input type="text" name="username"><br><br>
+
+     password : <input type="password" name="password"><br><br>
+    <button type="submit">login</button><center>
 </form>
 
-<?php
-if(isset ($_POST["angka"])){
-    $newAngka = $_POST["angka"];
-    for ($i = 1; $i <= $newAngka; $i++) {
-        if ($i % 2 == 0) {
-            echo "$i ini adlah angka (genap) <br>";
-        }else {
-            echo "$i ini adalah angka (ganjil) <br>";
-        }
-    }
-}
+</BODY>
 
-?>
+</HTML>
